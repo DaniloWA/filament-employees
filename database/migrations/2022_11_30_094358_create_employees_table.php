957285b3426913ea4f3dc6f859853f9de409a63e
Danilo Oliveira <Daniloworkdev@gmail.com>
2022-11-30 10:13:44 +0000

    feat: make migrate Employees and implements logic

---
+++ database/migrations/2022_11_30_094358_create_employees_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('country_id');
+            $table->unsignedBigInteger('state_id');
+            $table->unsignedBigInteger('city_id');
+            $table->unsignedBigInteger('department_id');
+
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('address');
+            $table->char('zip_code');
+
+            $table->date('birth_date');
+            $table->date('date_hired');
+
+            $table->timestamps();
+
+
+            $table->foreign('country_id')->references('id')->on('contries')->onDelete('cascade');
+            $table->foreign('state_id')->references('id')->on('states')->onDelete('cascade');
+            $table->foreign('city_id')->references('id')->on('cities')->onDelete('cascade');
+            $table->foreign('department_id')->references('id')->on('departments')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+};
